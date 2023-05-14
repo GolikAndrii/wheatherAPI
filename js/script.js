5baceac9c216949f218f21dcdd88c48f475ca530
Golik Andrii <Golik.Andrii@gmail.com>
2023-05-14 10:46:17 +0200

    Add toggle for Night mode

--- js/script.js
+++ js/script.js
@@ -19,21 +19,32 @@ let getWeather = ()=>{
                 console.log(data.main)
                 console.log(data.main.temp_min)
                 console.log(data.main.temp_max)
+                console.log(data.main.humidity)
+                console.log(data.wind.speed)
                 result.innerHTML = `
-                    <h2>${data.name}</h2>
-                    <h4 class="weather">${data.weather[0].main}</h4>
-                    <h4 class="desc">${data.weather[0].description}</h4>
-                    <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+                    <h2>${data.name}</h2>                    
+                    <div class="temperature-img">
+                        <div>
+                            <h4 class="weather">${data.weather[0].main}</h4>
+                            <h4 class="desc">${data.weather[0].description}</h4>
+                        </div>    
+                        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+                    </div>
                     <h1>${Math.round(data.main.temp)} &#176;</h1>
                     <div class="temp-container">
                         <div>
-                        <h4 class="title">min</h4>
-                        <h4 class="temp">${Math.round(data.main.temp_min)} &#176;</h4>
+                            <h4 class="temp"><span class="wind">wind speed:</span> ${data.wind.speed} km/h</h4>
+                            <h4 class="temp"><span class="humidity">humidity:</span> ${data.main.humidity} %</h4>
                         </div>
                         <div>
-                        <h4 class="title">max</h4>
-                        <h4 class="temp">${Math.round(data.main.temp_max)} &#176;</h4>
+                            <h4 class="title">min</h4>
+                            <h4 class="temp">${Math.round(data.main.temp_min)} &#176;</h4>
                         </div>
+                        <div>
+                            <h4 class="title">max</h4>
+                            <h4 class="temp">${Math.round(data.main.temp_max)} &#176;</h4>
+                        </div>
+
                     </div>
                 `
             })
